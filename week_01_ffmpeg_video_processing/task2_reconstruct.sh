e5e0c296ec9de547e6bb9dc666721bd3692c2054
rashmikanna <rashmik4257@gmail.com>
2026-04-25 12:11:23 +0000

    Week 01 Task 2: Extracted 1800 frames and reconstructed 30fps video(updated)

--- week_01_ffmpeg_video_processing/task2_reconstruct.sh
+++ week_01_ffmpeg_video_processing/task2_reconstruct.sh
@@ -1,1 +1,1 @@
-ffmpeg -framerate 30 -i ../data/output_frames/task2_frame_%04d.jpg -c:v libx264 -pix_fmt yuv420p ../data/output_frames/task2_reconstructed.mp4
+ffmpeg -framerate 30 -i ../data/output_frames/task2_frame_%04d.jpg -c:v libx264 -crf 18 -pix_fmt yuv420p ../data/output_frames/task2_reconstructed_hq.mp4
